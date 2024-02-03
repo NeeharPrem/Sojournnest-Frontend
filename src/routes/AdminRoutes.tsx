0b0082ee3f26b0b3e9e4aa7e330a2d85650b65ef
NeeharPrem <neeharprem94@gmail.com>
2024-02-03 19:54:41 +0530

    Admin and User routes updated to handle new routes

--- src/routes/AdminRoutes.tsx
+++ src/routes/AdminRoutes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import AdminDash from "../pages/admin/AdminDash";
 import { AdminUsers } from "../pages/admin/AdminUsers";
 import { AdminListings } from "../pages/admin/AdminListing";
+import { Amenities } from "../pages/admin/Amenities";
 
 const AdminRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AdminRoutes = () => {
       <Route path="/dashboard" element={<AdminDash />} />
       <Route path="/users" element={<AdminUsers />} />
       <Route path="/listings" element={<AdminListings />} />
+      <Route path="/facilities" element={<Amenities />} />
     </Routes>
   );
 };
--- src/routes/userRoutes.tsx
+++ src/routes/userRoutes.tsx
@@ -1,17 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import SignUp from "../pages/user/userLogin";
-import UserHome from "../pages/user/home";
 import UserProfile from "../pages/user/userProfile";
 import HostHome from "../pages/userHost/userHost";
 import SelectLocation from "../pages/userHost/SelectLocation";
+import { HostDash } from "../pages/userHost/HostDashboard";
 
 const UserRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<UserHome />} />
-      <Route path="signup" element={<SignUp />} />
       <Route path="/profile" element={<UserProfile />} />
-      <Route path="/login" element={<SignUp />}></Route>
+      <Route path="/Dashboard" element={<HostDash />}></Route>
       <Route path="/host" element={<HostHome />}></Route>
       <Route path="/location" element={<SelectLocation />}></Route>
       <Route path="/editRoom/:roomId" element={<SelectLocation />}></Route>
