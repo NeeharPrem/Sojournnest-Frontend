9948db2212191d226903d910309c3ab3ed34c25d
NeeharPrem <neeharprem94@gmail.com>
2024-02-07 10:37:11 +0530

    Redux store updated to Handle new usecase

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slice/authSlice";
-import modalReducer from "./slice/modalSlice";
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
-    modal: modalReducer,
+    auth: authReducer
   },
   devTools: true,
 });
