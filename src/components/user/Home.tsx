0d64198fe8603ada2307b7abef3a8f430700141f
NeeharPrem <neeharprem94@gmail.com>
2024-02-09 20:00:09 +0530

    Home page added and react skeleton for Home page  added for better UX

--- src/components/user/Home.tsx
+++ src/components/user/Home.tsx
@@ -1,43 +1,50 @@
 import { useQuery } from "@tanstack/react-query";
 import { allListings } from "../../api/userapi";
-import Skeleton from "./HomeSkeleton";
+import { useNavigate } from "react-router-dom";
+import SkeletonLoader from "./HomeSkeleton";
+import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 
 interface Room {
   name: string;
   _id: string;
   images: string[];
-  state:string
+  state: string;
+  district: string;
+  rent: string;
 }
 
 const HomePage = () => {
-  const { data: Data } = useQuery({
+  const { data: Data,isLoading} = useQuery({
     queryKey: ["roomData"],
     queryFn: allListings,
   });
 
+  const navigate = useNavigate()
+
   return (
-    <div className="bg-white min-h-screen">
-      <main className="container mx-auto py-8 flex flex-col items-center">
-        <div className="flex justify-center flex-wrap gap-3">
-          {Data?.data?.map((room: Room) => (
-            <div key={room._id} className="w-full sm:w-64 md:w-72 lg:w-96 bg-white rounded-lg overflow-hidden shadow-md border border-gray-300 p-4">
-              <img src={room.images[0]} alt="Card" className="w-full h-48 object-cover" />
-              <div className="p-4">
-                <h2 className="text-xl font-bold mb-2">{room.name || <Skeleton />}</h2>
-                <p className="text-gray-700">{room?.state}</p>
-                {/* <div className="mt-4">
-                  <a href="#" className="text-blue-500 hover:underline">Read More</a>
-                </div> */}
+    <>
+        <div id="banner" className="w-full justify-center items-center">
+          <img src="https://a0.muscache.com/im/pictures/hosting/Hosting-997566368472977053/original/2b023175-6872-4202-ace0-29bc06504385.jpeg?im_w=960" alt="Card" className="w-full lg:h-72 object-cover" />
+        </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-5 my-7  items-center">
+        {isLoading ? (
+          Array.from({ length: 8 }).map((_, index) => <SkeletonLoader key={index} />)
+        ) : (
+          Data?.data?.map((room: { _id: Key | null | undefined; images: (string | undefined)[]; name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined; state: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; district: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; rent: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }) => (
+            <div key={room._id} onClick={() => navigate(`/details/${room._id}`)} className="w-full cursor-pointer transition-all duration-300 transform hover:scale-105 shadow-lg overflow-hidden">
+              <img src={room.images[0]} alt="card" className="w-full h-40 object-cover" />
+              <div className="flex flex-col p-4">
+                <h2 className="text-xl font-bold mb-2">{room.name}</h2>
+                <p className="text-gray-700">{room.state}, {room.district}</p>
+                <p className="text-gray-700">₹ {room.rent} / night</p>
               </div>
             </div>
-          ))}
+          ))
+        )}
         </div>
-      </main>
-      <footer className="bg-blue-500 p-4 text-white text-center">
-        <p>&copy; 2023 Hotel Booking. All rights reserved.</p>
-      </footer>
-    </div>
+
+    </>
   );
 };
-
 export default HomePage;
