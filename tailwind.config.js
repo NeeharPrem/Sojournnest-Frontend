d18d82c53c74b0a7a8f78faceb94d4a87842c579
NeeharPrem <neeharprem94@gmail.com>
2024-02-14 15:32:58 +0530

    Custom color added to the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      'cream': '#F1F5F9',
+    },
     extend: {},
   },
   plugins: [],
