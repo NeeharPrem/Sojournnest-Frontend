f62e2e12af639ffe8e4805c2c5f161cdb5900e34
NeeharPrem <neeharprem94@gmail.com>
2024-02-17 05:54:34 +0530

    Enhance Conversation UI with last message timing and visual improvements
    - Implement display of last message sent or received time for better user context

--- src/components/userHost/Messenger/Conversations.tsx
+++ src/components/userHost/Messenger/Conversations.tsx
@@ -8,9 +8,9 @@ export interface User {
 }
 
 interface Message {
+    createdAt: Date; // Consider using a more specific type than 'any', like Date or string
     conversationId: string;
     text: string;
-    // Add other properties like sender, timestamp etc. as needed
 }
 
 interface ConversationListProps {
@@ -22,52 +22,92 @@ interface ConversationListProps {
     lastMsg: Message[][];
 }
 
+interface LastMessageInfo {
+    text: string;
+    createdAt?: Date; // Make this optional since there might not be a last message
+}
+
 const ConversationList: React.FC<ConversationListProps> = ({ usersWithConvoId, onConversationSelect, lastMsg }) => {
     const handleConversationClick = (userId: string, conversationId: string) => {
-        console.log(`Conversation with user ${userId} clicked.`);
         onConversationSelect(userId, conversationId);
     };
 
-
-    const findLastMessage = (conversationId: string): string => {
+    const findLastMessage = (conversationId: string): LastMessageInfo | string => {
         if (!lastMsg || lastMsg.length === 0) {
             return "No messages yet";
         }
         const conversationMessages = lastMsg.find(messages => messages?.some(message => message.conversationId === conversationId));
         if (conversationMessages && conversationMessages.length > 0) {
             const lastMessage = conversationMessages[conversationMessages.length - 1];
-            return lastMessage.text;
+            return {
+                text: lastMessage.text,
+                createdAt: lastMessage.createdAt
+            };
         }
         return "No messages yet";
     };
 
+    const formatDate = (dateInput: Date | string): string => {
+        let date = new Date(dateInput);
+        if (!(date instanceof Date) || isNaN(date.getTime())) {
+            // If `dateInput` does not represent a valid date, handle the error
+            console.error('Invalid date:', dateInput);
+            return 'Invalid date';
+        }
+
+        const now = new Date();
+        const today = new Date(now.setHours(0, 0, 0, 0));
+        const yesterday = new Date(today);
+        yesterday.setDate(yesterday.getDate() - 1);
+        const weekAgo = new Date(today);
+        weekAgo.setDate(weekAgo.getDate() - 7);
+
+        if (date >= today) {
+            return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        } else if (date >= yesterday) {
+            return "Yesterday";
+        } else if (date >= weekAgo) {
+            return date.toLocaleDateString([], { weekday: 'long' });
+        } else {
+            return date.toLocaleDateString([], { day: '2-digit', month: '2-digit', year: 'numeric' });
+        }
+    };
 
     return (
-        <div className="flex flex-col h-screen bg-gray-100">
-            <div className="bg-blue-gray-300 p-4 shadow-md h-16">
-                <h1 className="text-lg font-semibold">Conversations</h1>
+        <>
+            <div className="bg-blue-gray-300 p-4">
+                <h1 className="text-lg font-semibold">Chats</h1>
             </div>
-            <ul className="flex-grow overflow-y-auto">
-                {usersWithConvoId.map(({ user, conversationId }) => (
-                    <li key={user._id} className="flex items-center p-4 border-b border-gray-200">
-                        <button
-                            onClick={() => handleConversationClick(user._id, conversationId)}
-                            className="flex items-center p-4 w-full text-left"
-                        >
-                            <img
-                                className="w-12 h-12 rounded-full object-cover mr-4"
-                                src={user.profilePic || "https://source.unsplash.com/random"}
-                                alt="User"
-                            />
-                            <div>
-                                <h2 className="text-lg font-semibold">{user.fname} {user.lname}</h2>
-                                <p className="text-gray-600">{findLastMessage(conversationId)}</p>
-                            </div>
-                        </button>
-                    </li>
-                ))}
+
+            <ul className="flex-grow overflow-y-auto w-64">
+                {usersWithConvoId.map(({ user, conversationId }) => {
+                    const lastMessage = findLastMessage(conversationId);
+                    const lastMessageText = typeof lastMessage === 'string' ? lastMessage : lastMessage.text;
+                    const lastMessageDate = typeof lastMessage === 'object' && lastMessage.createdAt ? formatDate(lastMessage.createdAt) : '';
+                    return (
+                        <li key={user._id} className="flex items-center p-4 border-b border-gray-200 hover:bg-gray-50 transition-colors duration-150 ease-in-out">
+                            <button
+                                onClick={() => handleConversationClick(user._id, conversationId)}
+                                className="flex items-center p-4 w-full text-left focus:outline-none"
+                            >
+                                <img
+                                    className="w-12 h-12 rounded-full object-cover mr-4"
+                                    src={user.profilePic || "https://source.unsplash.com/random"}
+                                    alt="User"
+                                />
+                                <div className="flex-grow min-w-0">
+                                    <h2 className="text-sm font-semibold truncate">{user.fname} {user.lname}</h2>
+                                    <p className="text-gray-600 text-xs truncate">{lastMessageText}</p>
+                                </div>
+                                <div className="whitespace-nowrap">
+                                    <p className="text-green-600 text-xs">{lastMessageDate}</p>
+                                </div>
+                            </button>
+                        </li>
+                    );
+                })}
             </ul>
-        </div>
+        </>
     );
 };
 
