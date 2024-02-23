1b7ce1826d80a64f612a691f39669d94e18ba53c
NeeharPrem <neeharprem94@gmail.com>
2024-02-23 18:57:26 +0530

    Page for showing wihslist of user

---
+++ src/pages/user/wishlist/wishlist.tsx
@@ -0,0 +1,17 @@
+import NavBar from "../../../components/Navbar";
+import Wish from "../../../components/user/wish.tsx/wish";
+const Wishlist = () => {
+
+    return (
+        <>
+            <NavBar />
+            <div className="flex flex-col lg:px-16 px-5 sm:px-10 md:px-10 pt-20">
+                <div className="lg:mt-4">
+                    <Wish />
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Wishlist;
