6d24576f2a0949dde5d5e7d1289a8f2de5408d3a
NeeharPrem <neeharprem94@gmail.com>
2024-02-27 11:38:39 +0530

    feat: page for bookings added for user

---
+++ src/pages/user/bookings/bookings.tsx
@@ -0,0 +1,17 @@
+import NavBar from "../../../components/Navbar";
+
+const Bookings = () => {
+
+    return (
+        <>
+            <NavBar />
+            <div className="flex flex-col lg:px-16 px-5 sm:px-10 md:px-10 pt-20">
+                <div className="lg:mt-4">
+                   
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Bookings;
