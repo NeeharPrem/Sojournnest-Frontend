bd329cd9ee5f7029f80c316de47ac70cb886d191
NeeharPrem <neeharprem94@gmail.com>
2024-02-29 11:10:14 +0530

    feat: page for handling booking in the user side

--- src/components/user/bookings/bookings.tsx
+++ src/components/user/bookings/bookings.tsx
@@ -1,12 +1,27 @@
-import { useQuery, useMutation } from "@tanstack/react-query"
+import { useQuery,useMutation} from "@tanstack/react-query";
 import Loader from "../../common/Loader";
-import { toast } from "react-toastify";
-import { Link,useLocation} from "react-router-dom";
-import { useState } from "react";
+import { getBookings,cancelBooking} from "../../../api/userapi";
+import { Link } from "react-router-dom";
+import {useState } from "react";
 import Cancelled from "./cancelled";
 
+interface BookingItem {
+    _id: string;
+    roomId: {
+        images: string[];
+        name: string;
+        bedrooms:string;
+        guests:string;
+        bathrooms:string;
+    };
+    checkInDate: string;
+    checkOutDate: string;
+}
+
 const Booking = () => {
-    const [isUpcoming, setIsUpcoming] = useState(true); 
+    const [isUpcoming, setIsUpcoming] = useState(true);
+
+   
 
     const handleUpcomingClick = () => {
         setIsUpcoming(true);
@@ -15,87 +30,93 @@ const Booking = () => {
     const handleCancelledClick = () => {
         setIsUpcoming(false);
     };
-    // const {
-    //     data: Data, isLoading
-    // } = useQuery({
-    //     queryKey: ["wishListData",],
-    //     queryFn: userWishlists,
-    // });
 
-    // const { mutate: removWishlist } = useMutation({
-    //     mutationFn: removeWishlist,
-    //     onSuccess: (response) => {
-    //         if (response?.status === 200) {
-    //             toast.success(response.data.message)
-    //         }
-    //     }
-    // })
+    const { data: bookingsData, isLoading,refetch} = useQuery({
+        queryKey: ["bookingsData"],
+        queryFn: getBookings,
+    });
 
-    // const handleRemove = (event: React.MouseEvent<HTMLButtonElement>) => {
-    //     const button = event.target as HTMLButtonElement;
-    //     const Id = button.getAttribute('data-id') || '';
-    //     console.log('am hee', Id);
-    //     removWishlist(Id);
-    // };
-    
+     const { mutate: cancelledData } = useMutation({
+        mutationFn: cancelBooking,
+        onSuccess: (response) => {
+            console.log(response)
+            refetch()
+        },
+    });
+
+    const handlBooking=(id:string)=>{
+        cancelledData(id)
+    }
+
+    const formatDate = (dateString: string) => {
+        const options: Intl.DateTimeFormatOptions = { day: '2-digit', month: 'short', year: 'numeric' };
+        const date = new Date(dateString);
+        return date.toLocaleDateString('en-GB', options).replace(/\s/g, '-');
+    }
+
+    if (isLoading) {
+        return <Loader />;
+    }
+
     return (
         <>
             <div className="px-5">
                 <Link to="/profile">Profile</Link> / Bookings
             </div>
             <div className="lg:px-5">
-                {isUpcoming ? (<p className="font-bold text-custom-size">Your Bookings</p>):
-                    (<p className="font-bold text-custom-size">Cancelled Bookings</p>)
-                }
+                <p className="font-bold text-custom-size">{isUpcoming ? 'Your Bookings' : 'Cancelled Bookings'}</p>
             </div>
             <div className="px-5">
-              <div className="flex flex-row gap-4 border-b">
-                    <div className="border-b pb-2">
-                        <button onClick={handleUpcomingClick} className={`font-bold ${isUpcoming ? ('text-gray-600') : ('text-blue-500')}`}>
+                <div className="flex flex-row gap-4 border-b">
+                    <div className={`border-b pb-2 ${isUpcoming ? 'text-gray-600' : 'text-blue-500'}`}>
+                        <button onClick={handleUpcomingClick} className="font-bold">
                             Upcoming
                         </button>
                     </div>
-                    <div className="border-b pb-2">
-                        <button onClick={handleCancelledClick} className={`font-bold ${!isUpcoming ? 'text-gray-600' : 'text-blue-500'}`}>
+                    <div className={`border-b pb-2 ${!isUpcoming ? 'text-gray-600' : 'text-blue-500'}`}>
+                        <button onClick={handleCancelledClick} className="font-bold">
                             Cancelled
                         </button>
                     </div>
-              </div>
+                </div>
             </div>
-            {isUpcoming ?(
-                <div className="flexbg-blue-gray-500 mt-5 px-5">
-                    <div className="flex flex-row justify-between items-center p-5 w-full shadow-md rounded-md border">
-                        <div className="flex flex-row gap-2">
+            {isUpcoming ? (
+                bookingsData?.data.map((room: BookingItem) => (
+                    <div key={room._id} className="flex mt-5 px-5">
+                        <div className="flex flex-row justify-between items-center p-5 w-full shadow-md rounded-md border">
+                            <div className="flex flex-row gap-4">
+                                <div>
+                                    <img
+                                        src={room.roomId.images[0]}
+                                        alt="Room"
+                                        className="lg:w-20 lg:h-20 object-cover rounded-md"
+                                    />
+                                </div>
+                                <div className="lg:pt-1">
+                                    <div>
+                                        <p className="font-bold">{room.roomId.name}</p>
+                                    </div>
+                                    <div className="flex flex-row gap-1">
+                                        <p>{room.roomId.bedrooms} Beds . {room.roomId.guests} Guests . {room.roomId.bathrooms} Bathrooms</p>
+                                    </div>
+                                    <div className="flex flex-row text-black mt-1">
+                                        {formatDate(room.checkInDate)} - {formatDate(room.checkOutDate)}
+                                    </div>
+                                </div>
+                            </div>
                             <div>
-                                <img
-                                    src="https://a0.muscache.com/im/pictures/miso/Hosting-821668347227918484/original/71e0d8e0-3dee-414f-850f-018c42a8f460.jpeg?im_w=960"
-                                    alt="Profile Pic"
-                                    className="lg:w-20 lg:h-20  object-cover rounded-md"
-                                />
+                                <button onClick={()=>handlBooking(room._id)} className="border p-1 rounded-md bg-blue-gray-200">
+                                    Cancel booking
+                                </button>
                             </div>
-                            <div className="lg:pt-3">
-                                <div className="flex flex-row gap-1">
-                                    <p>Room name</p>
-                                    {/* <p>{room.bedrooms} Bedrooms</p> */}
-                                </div>
-                                <div className="flex flex-row gap-1">
-                                    {/* <p>{room.guests} Guests .</p>
-                                    <p>{room.bathrooms} Bathrooms .</p> */}
-                                </div>
-                            </div>
-                        </div>
-                        <div >
-                            <button className="border p-1 rounded-md bg-blue-gray-200">
-                                Cancel booking
-                            </button>
                         </div>
                     </div>
-                </div>
-            ):(
-                    <Cancelled/>
+                ))
+            ) : (
+                <Cancelled />
             )}
         </>
-    )
-}
+    );
+};
 
-export default Booking
+export default Booking;
