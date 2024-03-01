503f24259fb0cf756da0d4d9715410adb8c0c2ce
NeeharPrem <neeharprem94@gmail.com>
2024-03-01 17:17:36 +0530

    fix: component logic updated to show otp verify screen

--- src/components/user/SignUp.tsx
+++ src/components/user/SignUp.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate,Link} from "react-router-dom";
 import { toast } from "react-toastify";
 import { GoogleLogin } from "@react-oauth/google";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { jwtDecode } from "jwt-decode";
-import { login, signup } from "../../api/userapi";
+import { signup } from "../../api/userapi";
 import { useMutation } from "@tanstack/react-query";
-import { setLogin } from "../../store/slice/authSlice";
 import OTPScreen from "../common/otpEnter";
 
 const G_Password = import.meta.env.VITE_GOOGLE_PASSWORD;
@@ -22,23 +21,18 @@ const SignUp = () => {
   const [lname, setLname] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const [Cpassword, setCPassword] = useState<string>("");
-  const [isLogin, setIsLogin] = useState<boolean>(true);
   const [isComplete, setIscomplete] = useState(false);
 
-  const dispatch = useDispatch();
+
+
   const navigate = useNavigate();
   const { userLoggin } = useSelector((state: RootState) => state.auth);
-
-  const handleToggleMode = () => {
-    setIsLogin(!isLogin);
-  };
 
   useEffect(() => {
     if (userLoggin) {
       navigate("/");
     }
-  }, [userLoggin, isLogin]);
+  }, [userLoggin]);
 
   const { mutate: userSignupMutate } = useMutation({
     mutationFn: signup,
@@ -48,22 +42,6 @@ const SignUp = () => {
         setIscomplete(true);
         setEmail("");
         setPassword("");
-      }
-    },
-  });
-
-  const { mutate: userLoginMutate } = useMutation({
-    mutationFn: login,
-    onSuccess: (response) => {
-      if (response?.status == 200) {
-        toast.success("Login Successfull");
-        const data = {
-          id: response.data?.message?._id,
-          fname: response.data?.message?.fname,
-          lname: response.data?.message?.lname,
-        };
-        dispatch(setLogin({ userId: data.id,userLoggedin:data}));
-        navigate("/");
       }
     },
   });
@@ -84,31 +62,9 @@ const SignUp = () => {
       is_google: true,
     };
     const result = await signup(data);
-    console.log(result);
     if (result?.status == 200) {
       toast.success("Signup Successfull");
-      handleToggleMode();
-    }
-  };
-
-  const googleLogin = async (response: any) => {
-    const decode: { email: string } = jwtDecode(response.credential as string);
-    const data = {
-      email: decode.email,
-      password: G_Password,
-    };
-    const result = await login(data);
-    if (result?.status == 200) {
-      toast.success("Login Successfull");
-      const data = {
-        id: result.data.message._id,
-        fname: result.data.message.fname,
-        lname: result.data.message.lname,
-      };
-      dispatch(setLogin({ userId: data.id, userLoggedin: data }));
-      navigate("/");
-    } else {
-      console.log("Error");
+      navigate("/login");
     }
   };
 
@@ -152,101 +108,36 @@ const SignUp = () => {
     userSignupMutate(formData);
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = { email, password };
-    userLoginMutate(formData);
-  };
 
   return (
     <>
-      <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center mt-10 bg-opacity-50">
-        {isComplete ? (
-          <OTPScreen
-            setIscomplete={setIscomplete}
-            isLogin={isLogin}
-            setIsLogin={setIsLogin}
-          />
-        ) : (
-          <div className="bg-white p-8 border rounded-md max-w-md w-full">
-            <h2 className="text-2xl font-bold mb-4">
-              {isLogin ? "Login" : "Sign Up"}
-            </h2>
-              <form onSubmit={isLogin ? handleSubmit : handleSignupClick} noValidate>
-              {!isLogin && (
-                <div className="mb-4">
-                  <label className="block text-sm font-medium text-gray-600">
-                    First Name:
-                  </label>
-                  <input
-                    type="text"
-                    className="w-full border px-3 py-2 rounded-md"
-                    placeholder="Enter your first name"
-                    onChange={(e) => setFname(e.target.value)}
-                    required
-                  />
+    <div>
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 mt-10">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Create an account
+              </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSignupClick}>
+              <div>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
+                  <input type="text" name="fname" id="fname" onChange={(e) => setFname(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="First name" required />
                 </div>
-              )}
-              {!isLogin && (
-                <div className="mb-4">
-                  <label className="block text-sm font-medium text-gray-600">
-                    Last Name:
-                  </label>
-                  <input
-                    type="text"
-                    className="w-full border px-3 py-2 rounded-md"
-                    placeholder="Enter your last name"
-                    onChange={(e) => setLname(e.target.value)}
-                    required
-                  />
+                <div>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
+                  <input type="text" name="lname" id="lname" onChange={(e) => setLname(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Last name" required />
                 </div>
-              )}
-              <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-600">
-                  Email:
-                </label>
-                <input
-                  type="text"
-                  className="w-full border px-3 py-2 rounded-md"
-                  placeholder="Enter your email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-600">
-                  Password:
-                </label>
-                <input
-                  type="password"
-                  className="w-full border px-3 py-2 rounded-md"
-                  placeholder="Enter your password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-              </div>
-              <button
-                type="submit"
-                className="bg-blue-500 text-white px-4 py-2 rounded-md"
-              >
-                {isLogin ? "Login" : "Signup"}
-              </button>
-            </form>
-            {isLogin ? (
-              <div className="flex justify-center ">
-                <GoogleLogin
-                  onSuccess={(response) => {
-                    googleLogin(response);
-                  }}
-                  onError={() => {
-                    console.log("Login Failed");
-                  }}
-                />
-              </div>
-            ) : (
-              <div id="signup" className="flex justify-center ">
+                <div>
+                  <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                  <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                </div>
+                <div>
+                  <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                  <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                </div>
+                <button type="submit" className="w-full text-black bg-yellow-200 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+              </form>
+              <div className="flex justify-center">
                 <GoogleLogin
                   onSuccess={(response) => {
                     googleSignup(response);
@@ -256,15 +147,17 @@ const SignUp = () => {
                   }}
                 />
               </div>
-            )}
-            <p className="mt-4">
-              {isLogin
-                ? "Don't have an account? "
-                : "Already have an account? "}
-              <button className="text-blue-500" onClick={handleToggleMode}>
-                {isLogin ? "Sign up here" : "Login here"}
-              </button>
-            </p>
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Already have an account <Link to='/login'>Login</Link>
+                </p>
+            </div>
+          </div>
+        </div>
+        {isComplete && (
+          <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+            <OTPScreen
+              setIscomplete={setIscomplete}
+            />
           </div>
         )}
       </div>
