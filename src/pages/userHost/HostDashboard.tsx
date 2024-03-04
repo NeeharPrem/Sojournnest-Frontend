e85477edf24d8b96ef2a28021f9f6531e59b0aaa
NeeharPrem <neeharprem94@gmail.com>
2024-03-04 12:57:41 +0530

    Clean up: Removed unused imports and commands for better code clarity and performance

--- src/pages/userHost/HostDashboard.tsx
+++ src/pages/userHost/HostDashboard.tsx
@@ -1,4 +1,3 @@
-import Dashboard from "../../components/userHost/DashBar";
 import Navbar from "../../components/Navbar"
 import { Frame } from "../../components/userHost/hostDashboard";
 
@@ -6,8 +5,7 @@ export const HostDash = (): JSX.Element => {
     return (
        <>
             <Navbar />
-            <div className="flex flex-row lg:px-16 px-5 sm:px-10 md:px-10 pt-20">
-                {/* <Dashboard/> */}
+            <div className="flex flex-row sm:px-10 md:px-10 xs:px-10 px-5 pt-20 h-screen">
                 <Frame/>
             </div>
        </>
