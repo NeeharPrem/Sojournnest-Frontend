a232e8731716652788ec1cbc67ee793984e9c3d1
NeeharPrem <neeharprem94@gmail.com>
2024-03-05 11:51:43 +0530

    Refine UI elements on SelectLocation page for enhanced user experience

--- src/pages/userHost/SelectLocation.tsx
+++ src/pages/userHost/SelectLocation.tsx
@@ -11,7 +11,7 @@ const SelectLocation = () => {
   return (
     <>
       <Navbar />
-      <div className="flex justify-center mt-10">
+      <div className="flex flex-col justify-center lg:px-20 px-5 sm:px-10 md:px-10 pt-20">
         {isEditRoom ? <EditRoom Id={roomId} /> : <AddRoom />}
       </div>
     </>
