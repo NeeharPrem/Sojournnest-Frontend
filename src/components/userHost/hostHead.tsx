9d23cc7f34099eda9cd810eaa6839c623b417233
NeeharPrem <neeharprem94@gmail.com>
2024-03-05 11:53:20 +0530

    Refined the UI for better UX

--- src/components/userHost/hostHead.tsx
+++ src/components/userHost/hostHead.tsx
@@ -4,12 +4,10 @@ import { Link } from "react-router-dom";
 const HostHeader: React.FC = () => {
   return (
     <header className="bg-white  p-4">
-      <div className="flex justify-between items-center">
-        <button className="bg-blue-gray-600 text-white py-2 px-4 square-full">
-          button
-        </button>
+      <div className="flex w-full justify-between items-center">
+        <p className="text-3xl font-semibold tracking-wide">Your Listings</p>
         <Link to="/location">
-          <button className="bg-blue-gray-600 text-white py-2 px-4 square-full">
+          <button className="bg-gray-400 text-black p-2 max-w-56 rounded">
             Add a Listing
           </button>
         </Link>
