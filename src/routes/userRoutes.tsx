d7dae757b01736d2056be5240b7717bfdfabb751
NeeharPrem <neeharprem94@gmail.com>
2024-03-11 20:22:31 +0530

    update: implimented route for Reservations

--- src/routes/userRoutes.tsx
+++ src/routes/userRoutes.tsx
@@ -8,11 +8,13 @@ import Wishlist from "../pages/user/wishlist/wishlist";
 import Bookings from "../pages/user/bookings/bookings";
 import SuccessOrFailure from "../pages/user/success or failure/success";
 import ManageDate from "../pages/userHost/Date/ManageDate";
+import { Reservation } from "../pages/userHost/Reservation/Reservation";
 const UserRoutes = () => {
   return (
     <Routes>
       <Route path="/profile" element={<UserProfile />} />
       <Route path="/Dashboard" element={<HostDash />}></Route>
+      <Route path="/reservations" element={<Reservation />}></Route>
       <Route path="/host" element={<HostHome />}></Route>
       <Route path="/location" element={<SelectLocation />}></Route>
       <Route path="/editRoom/:roomId" element={<SelectLocation />}></Route>
