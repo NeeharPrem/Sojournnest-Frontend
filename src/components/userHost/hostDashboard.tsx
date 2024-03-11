b72138377660f050d5736ded0f5daa306a9382fa
NeeharPrem <neeharprem94@gmail.com>
2024-03-11 21:18:25 +0530

    refactor: total refactor of the code for modularity and readability

--- src/components/userHost/hostDashboard.tsx
+++ src/components/userHost/hostDashboard.tsx
@@ -1,42 +1,9 @@
-import { Link } from 'react-router-dom';
-import { FaHome } from "react-icons/fa";
-import { TbBrandBooking } from "react-icons/tb";
-import { AiTwotoneMessage } from "react-icons/ai";
-import { LuTableProperties } from "react-icons/lu";
+import Sidebar from './Reservations/Sidebar';
 export const Frame = (): JSX.Element => {
     return (
         <>
             <div className='flex flex-row w-full h-full'>
-                <div className='flex flex-col w-80 lg:p-4 justify-between'>
-                    <div className='flex flex-col lg:gap-3 '>
-                        <Link to='/' className='hover:cursor-pointer '>
-                            <div className='flex flex-row items-center gap-2 max-w-60 hover:shadow-md p-2 rounded-md item-center'>
-                                <FaHome />
-                                <p> Home</p>
-                            </div>
-                        </Link>
-                        <Link to='/' className='hover:cursor-pointer '>
-                            <div className='flex max-w-60 flex-row items-center gap-2 hover:shadow-md p-2 rounded-md item-center'>
-                                <TbBrandBooking />
-                                <p>Resevations</p>
-                            </div>
-                        </Link>
-                        <Link to='/chats' className='hover:cursor-pointer '>
-                            <div className='flex flex-row items-center gap-2 max-w-60 hover:shadow-md p-2 rounded-md item-center'>
-                                <AiTwotoneMessage />
-                                <p>Messages</p>
-                            </div>
-                        </Link>
-                    </div>
-                    <div className='flex flex-col lg:gap-3 '>
-                        <Link to='/host' className='hover:cursor-pointer '>
-                            <div className='flex flex-row items-center gap-2 max-w-60 hover:shadow-md p-2 rounded-md item-center'>
-                                <LuTableProperties />
-                                <p> Listings</p>
-                            </div>
-                        </Link>
-                    </div>
-                </div>
+                <Sidebar/>
                 <div className='flex w-full lg:p-4'>
                     <div className='flex flex-col h-full w-full '>
                         <p className="text-3xl font-semibold tracking-wide">Reservation</p>
