f0e7ac36e3e211301980af08715545456dc87a2f
NeeharPrem <neeharprem94@gmail.com>
2024-03-15 10:21:19 +0530

    feat(host-section): Add component for displaying cancelled bookings
    - Introduced a new component to list all cancelled bookings, enabling hosts to easily view such reservations.
    - Utilized React Query for data fetching, ensuring up-to-date information is displayed.
    - Implemented a custom date formatting function to enhance readability of the booking period.

--- src/components/user/bookings/cancelled.tsx
+++ src/components/user/bookings/cancelled.tsx
@@ -1,27 +1,65 @@
+import Loader from "../../common/Loader";
+import { useQuery } from "@tanstack/react-query";
+import { cancelledBookings } from "../../../api/userapi";
+
+interface BookingRoom {
+    images: string[];
+    name: string;
+    bedrooms: number;
+    guests: number;
+    bathrooms: number;
+}
+
+interface BookingItem {
+    _id: string;
+    roomId: BookingRoom;
+    checkInDate: string;
+    checkOutDate: string;
+}
+
 const Cancelled = () => {
+
+    const { data: bookingsData, isLoading, refetch } = useQuery({
+        queryKey: ["cancelledData"],
+        queryFn: cancelledBookings,
+    });
+
+    const formatDate = (dateString: string) => {
+        const options: Intl.DateTimeFormatOptions = { day: '2-digit', month: 'short', year: 'numeric' };
+        const date = new Date(dateString);
+        return date.toLocaleDateString('en-GB', options).replace(/\s/g, '-');
+    }
+
+    if (isLoading) return <Loader/>;
+
   return (
       <div className="flexbg-blue-gray-500 mt-5 px-5">
-          <div className="flex flex-row justify-between items-center p-5 w-full shadow-md rounded-md border">
-              <div className="flex flex-row gap-2">
-                  <div>
-                      <img
-                          src="https://a0.muscache.com/im/pictures/miso/Hosting-821668347227918484/original/71e0d8e0-3dee-414f-850f-018c42a8f460.jpeg?im_w=960"
-                          alt="Profile Pic"
-                          className="lg:w-20 lg:h-20  object-cover rounded-md"
-                      />
-                  </div>
-                  <div className="lg:pt-3">
-                      <div className="flex flex-row gap-1">
-                          <p>Room name</p>
-                          {/* <p>{room.bedrooms} Bedrooms</p> */}
-                      </div>
-                      <div className="flex flex-row gap-1">
-                          {/* <p>{room.guests} Guests .</p>
-                                    <p>{room.bathrooms} Bathrooms .</p> */}
+          {bookingsData?.data?.map((room: BookingItem) => (
+              <div key={room._id} className="flex mt-5 px-5">
+                  <div className="flex flex-row justify-between items-center p-5 w-full shadow-md rounded-md border">
+                      <div className="flex flex-row gap-4">
+                          <div>
+                              <img
+                                  src={room.roomId.images[0]}
+                                  alt="Room"
+                                  className="lg:w-20 lg:h-20 object-cover rounded-md"
+                              />
+                          </div>
+                          <div className="lg:pt-1">
+                              <div>
+                                  <p className="font-bold">{room.roomId.name}</p>
+                              </div>
+                              <div className="flex flex-row gap-1">
+                                  <p>{room.roomId.bedrooms} Beds · {room.roomId.guests} Guests · {room.roomId.bathrooms} Bathrooms</p>
+                              </div>
+                              <div className="flex flex-row text-black mt-1">
+                                  {formatDate(room.checkInDate)} - {formatDate(room.checkOutDate)}
+                              </div>
+                          </div>
                       </div>
                   </div>
               </div>
-          </div>
+          ))}
       </div>
   )
 }
