dfa1a5cd3f83f3479e9fe9222a1785b96c274a06
NeeharPrem <neeharprem94@gmail.com>
2024-03-18 10:04:17 +0530

    feat: Added route for showing bookings page

--- src/routes/AdminRoutes.tsx
+++ src/routes/AdminRoutes.tsx
@@ -3,6 +3,7 @@ import AdminDash from "../pages/admin/AdminDash";
 import { AdminUsers } from "../pages/admin/AdminUsers";
 import { AdminListings } from "../pages/admin/AdminListing";
 import { Amenities } from "../pages/admin/Amenities";
+import { AdminBookings } from "../pages/admin/AdminBookings";
 
 const AdminRoutes = () => {
   return (
@@ -11,6 +12,7 @@ const AdminRoutes = () => {
       <Route path="/users" element={<AdminUsers />} />
       <Route path="/listings" element={<AdminListings />} />
       <Route path="/facilities" element={<Amenities />} />
+      <Route path='/bookings' element={<AdminBookings/>}/>
     </Routes>
   );
 };
