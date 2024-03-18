ff39020875f50716711c56c40f746e1d8a711779
NeeharPrem <neeharprem94@gmail.com>
2024-03-18 10:05:06 +0530

    feat: Page for showing all bookings

---
+++ src/pages/admin/AdminBookings.tsx
@@ -0,0 +1,10 @@
+import BookingTable from "../../components/admin/BookingTable";
+import AdminUiBase from "../../components/common/Admin/AdminUiBase";
+
+export const AdminBookings = () => {
+    return (
+        <AdminUiBase>
+            <BookingTable/>
+        </AdminUiBase>
+    );
+};
