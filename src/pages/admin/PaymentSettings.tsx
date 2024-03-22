1646744038682b9979bcd9dc944d8540a3a91041
NeeharPrem <neeharprem94@gmail.com>
2024-03-22 19:12:34 +0530

    feat: admin payment settings page
    - this page is used to show the admin payment settings

---
+++ src/pages/admin/PaymentSettings.tsx
@@ -0,0 +1,10 @@
+import Payments from "../../components/admin/Payments";
+import AdminUiBase from "../../components/common/Admin/AdminUiBase";
+
+export const PaymentSettings = () => {
+    return (
+        <AdminUiBase>
+            <Payments/>
+        </AdminUiBase>
+    );
+};
