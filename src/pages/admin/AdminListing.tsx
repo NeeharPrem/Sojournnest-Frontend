42cc436892be70f225f111f27305f7a633736f28
NeeharPrem <neeharprem94@gmail.com>
2024-04-06 19:40:41 +0530

    update: Removed the fetch logic from the page component
    - Fetch logic added directly to the table component it self

--- src/pages/admin/AdminListing.tsx
+++ src/pages/admin/AdminListing.tsx
@@ -1,20 +1,10 @@
 import ListingTable from "../../components/admin/ListingTable";
-import { useQuery } from "@tanstack/react-query";
-import { allListings } from "../../api/adminapi";
 import AdminUiBase from "../../components/common/Admin/AdminUiBase";
 
 export const AdminListings = () => {
-  const { data: Data, refetch } = useQuery({
-    queryKey: ["RoomData"],
-    queryFn: allListings,
-  });
   return (
      <AdminUiBase>
-      <ListingTable
-      refetch={refetch}
-        info={Data?.data.info}
-      userInfos={Data?.data.data || []}
-      />
+      <ListingTable/>
     </AdminUiBase>
   );
 };
