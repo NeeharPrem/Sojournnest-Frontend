9aa2673328f7c1397993220db1c9966fc4da2485
NeeharPrem <neeharprem94@gmail.com>
2024-04-06 19:23:02 +0530

    feat: Introduced booking slice
    - stores booking data for small amount time in the redux state
    - It will be removed when the payment completes

---
+++ src/store/slice/bookingSlice.ts
@@ -0,0 +1,51 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const loadInitialBookingState = () => {
+    const storedBooking = localStorage.getItem('booking');
+    return storedBooking ? JSON.parse(storedBooking) : null;
+};
+
+const initialBookingState = loadInitialBookingState() || {
+    roomName: '',
+    checkInDate: '',
+    checkOutDate: '',
+    guestsCount: 0,
+    roomRent: 0,
+    image:''
+};
+
+const bookingSlice = createSlice({
+    name: 'booking',
+    initialState: initialBookingState,
+    reducers: {
+        saveBookingDetails: (state, action) => {
+            const newState = { ...state, ...action.payload };
+            localStorage.setItem('booking', JSON.stringify(newState));
+            return newState;
+        },
+        clearBookingDetails: () => {
+            localStorage.removeItem('booking');
+            return {
+                roomName: '',
+                checkInDate: '',
+                checkOutDate: '',
+                guestsCount: 0,
+                roomRent: 0,
+                image:''
+            };
+        },
+    },
+});
+
+const store = configureStore({
+    reducer: {
+        booking: bookingSlice.reducer,
+    },
+});
+
+setTimeout(() => {
+    store.dispatch(bookingSlice.actions.clearBookingDetails());
+}, 1800000);
+
+export const { saveBookingDetails, clearBookingDetails } = bookingSlice.actions;
+export default bookingSlice.reducer;
