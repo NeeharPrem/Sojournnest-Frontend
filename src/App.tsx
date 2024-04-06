4b72a1468a931b2329357b51779bb51a927ef7ee
NeeharPrem <neeharprem94@gmail.com>
2024-04-06 20:25:31 +0530

    feat: Generate fcm token
    - code for generation fcm token implimented

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,39 @@ import { ListingDetails } from "./pages/user/listingDetails";
 import { AdminPrivate } from "./components/common/PrivateRoute";
 import { UserPrivate } from "./components/common/userPrivateRoute";
 import UserRoutes from "./routes/userRoutes";
+import { useEffect } from "react";
+import { generateToken, messaging } from "./firebase";
+import { onMessage } from "firebase/messaging";
+import { saveFcmtoken } from "./api/userapi";
+import { useSelector} from "react-redux";
+
+interface RootState {
+  auth: {
+    userLoggedin: boolean;
+  };
+}
 
 export default function App() {
+  const { userLoggedin } = useSelector((state: RootState) => state.auth);
+  useEffect(()=>{
+    generateToken().then((token) => {
+      if (token) {
+        if (userLoggedin) {
+          saveFcmtoken(token).then(() => {
+            console.log("Token sent to backend successfully");
+          }).catch((error) => {
+            console.error("Error sending token to backend:", error);
+          });
+        } else {
+          localStorage.setItem('fcmToken', token);
+          console.log("Token saved locally");
+        }
+      }
+    });
+    onMessage(messaging,(payload)=>{
+      console.log(payload)
+    })
+  },[])
   return (
     <>
       <Routes>
