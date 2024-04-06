1b30fe0fcf97134d57f08a0c4a93b75a0a67bd0c
NeeharPrem <neeharprem94@gmail.com>
2024-04-06 19:41:52 +0530

    update: fetch logic removed from page components
    - logic directly added to the table component it self

--- src/pages/admin/AdminUsers.tsx
+++ src/pages/admin/AdminUsers.tsx
@@ -1,23 +1,10 @@
 import UserTable from "../../components/admin/UserTable";
-import { useQuery } from "@tanstack/react-query";
-import { allUsers } from "../../api/adminapi";
 import AdminUiBase from "../../components/common/Admin/AdminUiBase";
 
 export const AdminUsers = () => {
-  const { data: Data, refetch } = useQuery({
-    queryKey: ["userData"],
-    queryFn: allUsers,
-  });
-
-  const userTableProps = {
-    refetch,
-    info: Data?.data.info,
-    userInfos: Data?.data.data || [],
-  };
-
   return (
     <AdminUiBase>
-      <UserTable {...userTableProps} />
+      <UserTable/>
     </AdminUiBase>
   );
 };
